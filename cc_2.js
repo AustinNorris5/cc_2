563cffa90de2f7b972b6fbee39250442ac2a2153
AustinNorris5 <ajnorris@usf.edu>
2025-01-26 14:07:39 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,9 @@
+//Task 1: Working with Arrays
+let products = ["Shirt", "Pants", "Shoes", "Hat", "Glasses"];
+
+products.push("Socks");
+
+products.pop();
+
+console.log("Updated Product List", products);
+
