214ee6eaa056ab42d676a5d6c8d695b451706834
AustinNorris5 <ajnorris@usf.edu>
2025-01-26 14:34:59 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -7,3 +7,13 @@ products.pop();
 
 console.log("Updated Product List", products);
 
+//Task 2: Accessing and Modifying Arrays
+let scores = [10,20,30,40,50];
+
+scores.splice(1, 1, 25);
+
+let total = scores.reduce((sum, scores) => sum + scores, 0);
+let average = total / scores.length;
+
+console.log("Updated Scores:", scores);
+console.log("Average Scores:", average);
